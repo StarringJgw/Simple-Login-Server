a1d82da6e2c420b9fcb5df69f0d3f1171910592b
Arch Jgw <780378026@qq.com>
2018-05-30 17:03:34 +0800

    2

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 var superagent = require('superagent');
 var cheerio = require('cheerio');
-superagent.get('http://bbs.mycraft.cc/forum-38-1.html')
+var http = require('http');
+superagent.get('http://bbs.mycraft.cc/thread-17373-1-1.html')
     .end(function (req, res) {
         // console.log(res.text);
         var $ = cheerio.load(res.text);
-        var links = $(".s,.xst");
-        for (var i = 0; i < links.length; i++) {
-            console.log(links[i].attribs.href);
+        var names = $(".pi>.authi>a");
+        var contents = $(".t_f")
+        for (var i = 0; i < names.length; i++) {
+            console.log(names[i].firstChild.data);
         }
-    })
+    });
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': ' text/plain' });
+    response.end('Hello');
+}).listen(8888);
+
+console.log('Server running at http://127.0.0.1:8888/');
