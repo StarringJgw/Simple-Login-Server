e601b7c90e8d975d9f98b7acd2f8c27957ebbaaf
StarringJgw 2 <780378026@qq.com>
2018-06-08 18:29:17 +0800

    beta

--- loginServer.js
+++ loginServer.js
@@ -10,8 +10,33 @@ app.post('/signin', function (req, res) {
     //console.log(req);
     console.log(req.body);
     //console.log(req.body.username);
-    var result = addUser(req.body.username, req.body.password, req.body.email);
-    res.json("123");
+    connection.query('insert into users(username,password,email) values(?,?,?);', [req.body.username, req.body.password, req.body.email],
+        function (err, rows, fields) {
+            //console.log('error' + err);
+            // console.log(req);
+            //return err; //怎么把这个error传出去！！！！！！！！！！！
+            res.json(err);
+            //console.log(rows);
+        })
+    //res.json("123");
+})
+app.post('/login', function (req, res) {
+    connection.query('select * from users where username =?', req.body.username, function (err, row, fields) {
+        if (row[0] && row[0].password === req.body.password) {
+            res.json("Login as " + row[0].username);
+        } else
+            res.json("Wrong Username or Password")
+    })
+})
+app.post('/findPassword', function (req, res) {
+    connection.query('select * from users where username =?', req.body.username, function (err, row, fields) {
+        if (row[0] && row[0].email === req.body.email) {
+            connection.query('update users set password=? where username=?', [req.body.password, req.body.username], function (err, row, fields) {
+                res.json(err);
+            })
+        } else
+            res.json("Wrong Username or Email")
+    })
 })
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -30,15 +55,22 @@ connection.connect(function (err) {
 //addUser("bbb", "bPass", "b@test.com");
 //printUsers();
 
-function addUser(username, password, email) {
-    return connection.query('insert into users(username,password,email) values(?,?,?);', [username, password, email], function (err, rows, fields) {
-        if (err) {
-            console.log('error' + err);
-            // console.log(req);
-            return err; //怎么把这个error传出去！！！！！！！！！！！
-        }
-        //console.log(rows);
-    })
+
+
+function addUser(username, password, email, res) {
+    async function finalReturn(err, rows, fields) {
+        return err;
+    }
+    return connection.query('insert into users(username,password,email) values(?,?,?);', [username, password, email],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('error' + err);
+                // console.log(req);
+                //return err; //怎么把这个error传出去！！！！！！！！！！！
+            }
+            //console.log(rows);
+        })
+
 }
 
 function printUsers() {
